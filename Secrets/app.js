d15438aabe967c1a590ee403e5ec044a496670bf
coderquill <coderquill@gmail.com>
2020-05-16 21:09:00 +0530

    Add encryption

--- Secrets/app.js
+++ Secrets/app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
 
 const app = express();
 
@@ -15,10 +17,13 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema ={
+const userSchema =new mongoose.Schema({
   email:String,
   password:String
-};
+});
+
+const secret = "Thisisoursecret.";
+userSchema.plugin(encrypt,{secret:secret, encryptedFiels:["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
