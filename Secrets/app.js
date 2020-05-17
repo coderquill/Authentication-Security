9a60541088c7af21d8efbb4a51d28bdbb9fbdefa
coderquill <coderquill@gmail.com>
2020-05-17 12:42:28 +0530

    Add hashing with salt

--- Secrets/app.js
+++ Secrets/app.js
@@ -5,7 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 //const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+//const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -42,9 +44,11 @@ app.get("/register",function(req, res){
 });
 
 app.post("/register",function(req, res){
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+  // Store hash in your password DB.
   const newUser = new User({
     email: req.body.username,
-    password: md5(req.body.password)
+    password: hash
   });
   newUser.save(function(err){
     if(err){
@@ -53,20 +57,24 @@ app.post("/register",function(req, res){
       res.render("secrets");
     }
   });
+  });
+
 });
 
 app.post("/login", function(req, res){
   const userName = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email:userName}, function(err, foundUser){
     if(err){
       console.log(err);
     }else{
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if(result == true){
+            res.render("secrets");
+          }
+        });
       }
     }
   });
